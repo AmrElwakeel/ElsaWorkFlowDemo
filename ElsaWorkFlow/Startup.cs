cd7d3feda502a134910c367e1adde4a8470c5fd8
Amr Elwakeel <Amr Elwakeel@MSI>
2022-05-25 13:04:39 +0200

    Add project files.

---
+++ ElsaWorkFlow/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elsa;
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.SqlServer;
+
+namespace ElsaWorkFlow
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var elsaSection = Configuration.GetSection("Elsa");
+            var SqlServerconnectionString = Configuration.GetConnectionString("Defualt");
+            // Elsa services.
+            services
+                .AddElsa(options => options.UseEntityFrameworkPersistence(ef => ef.UseSqlServer(SqlServerconnectionString))
+                    .AddConsoleActivities()
+                    .AddHttpActivities(elsaSection.GetSection("Server").Bind)
+                    .AddQuartzTemporalActivities()
+                    .AddWorkflowsFrom<Startup>()
+                );
+
+            // Elsa API endpoints.
+            services.AddElsaApiEndpoints();
+
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles()
+                .UseHttpActivities()
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+
+                    // For Dashboard.
+                    endpoints.MapFallbackToPage("/_Host");
+                });
+        }
+    }
+}
